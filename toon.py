4b60cc9fe3418f8e301f947fc1c68ab7791f0d94
James S. Wang <jjameswwang@gmail.com>
2021-08-06 11:42:56 -0400

    Add takeDamage and isDead

--- toon.py
+++ toon.py
@@ -1,3 +1,9 @@
 class Toon:
     def __init__(self):
         self.laff = 15
+
+    def takeDamage(self, damage):
+        self.laff -= damage
+
+    def isDead(self) -> bool:
+        return self.laff <= 0
