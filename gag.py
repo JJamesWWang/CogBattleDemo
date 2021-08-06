8d9d37832e4d81bee9dff8e8d21710d2ff6dbae1
James S. Wang <jjameswwang@gmail.com>
2021-08-06 16:10:55 -0400

    Move isGagHit to Gag as a static method

--- gag.py
+++ gag.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Gag:
     NONE, PASS, SQUIRT, THROW = range(4)
     DAMAGE = {
@@ -10,4 +13,12 @@ class Gag:
         SQUIRT: 0.9,
         THROW: 0.75
     }
+    
+    @staticmethod
+    def isGagHit(gag) -> bool:
+        rand = random.random()
+        isHit = rand < 0.95 and rand < Gag.CHANCE_TO_HIT[gag]
+        print(f"The toon {'hit' if isHit else 'missed'}")
+        return isHit
+
 
