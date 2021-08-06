01719dd8ec6487d896f42a858fedc2591d9e1c61
James S. Wang <jjameswwang@gmail.com>
2021-08-06 11:43:51 -0400

    Move damage, attacks, and chance to hit to static variables, remove executeAttack()

--- cog.py
+++ cog.py
@@ -4,20 +4,21 @@ import random
 class Cog:
     ATTACK_A = "Attack A"
     ATTACK_B = "Attack B"
+    ATTACKS = [ATTACK_A, ATTACK_B]
+    DAMAGE = {
+        ATTACK_A: 2,
+        ATTACK_B: 3
+    }
+    CHANCE_TO_HIT = {
+        ATTACK_A: 0.85,
+        ATTACK_B: 0.6
+    }
 
     def __init__(self):
         self.health = 12
-        self.attacks = [self.ATTACK_A, self.ATTACK_B]
-        self.damage = {
-            self.ATTACK_A: 2,
-            self.ATTACK_B: 3
-        }
 
     def takeDamage(self, damage):
         self.health -= damage
 
     def isDead(self):
         return self.health <= 0
-
-    def executeAttack(self):
-        attack = random.choice(self.attacks)
