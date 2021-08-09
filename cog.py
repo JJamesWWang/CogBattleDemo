efbba2f54f4084cafbc09c60a0bba4753efdaa18
James S. Wang <jjameswwang@gmail.com>
2021-08-09 17:32:15 -0400

    Modify based on changes to combatant.py

--- cog.py
+++ cog.py
@@ -4,7 +4,12 @@ from typing import List, Dict
 from overrides import overrides
 
 
-class Cog(Combatant):
+class Cog:
+    def __init__(self):
+        self.health = 12
+
+
+class CogCombatant(Combatant):
     """Represents a cog, an AI combatant.
 
     Currently attacks, damage, and chance to hit are hardcoded; however,
@@ -18,19 +23,31 @@ class Cog(Combatant):
     DAMAGE: Dict[str, int] = {ATTACK_A: 2, ATTACK_B: 3}
     CHANCE_TO_HIT: Dict[str, float] = {ATTACK_A: 0.85, ATTACK_B: 0.6}
 
-    def __init__(self, deterministic: bool = False) -> None:
-        self.health: int = 12
-        self.isDeterministic: bool = deterministic
+    def __init__(
+        self, battle: "CogBattle", cog: Cog, deterministic: bool = False
+    ) -> None:
+        super().__init__(battle, deterministic)
+        self.health: int = cog.health
+        self.selectedAttack: str = self.ATTACK_A
 
     @overrides
-    def executeAttack(self, targets: List[Combatant]):
+    def executeAttack(self):
         pass
 
-    def isCogHit(self, cogAttack: str) -> bool:
+    def selectAttack(self) -> str:
+        pass
+
+    @overrides
+    def isAttackHit(self) -> bool:
         isHit = (
             True
             if self.isDeterministic
-            else random.random() < Cog.CHANCE_TO_HIT[cogAttack]
+            else random.random()
+            < CogCombatant.CHANCE_TO_HIT[self.selectedAttack]
         )
         print(f"The cog {'hit' if isHit else 'missed'}")
         return isHit
+
+    def isCogHit(self, cogAttack):
+        self.selectedAttack = cogAttack
+        return self.isAttackHit()
--- toon.py
+++ toon.py
@@ -1,14 +1,32 @@
 from combatant import Combatant
+from gag import Gag
 from typing import List
 from overrides import overrides
+import random
 
 
-class Toon(Combatant):
+class Toon:
+    def __init__(self):
+        self.laff = 15
+
+
+class ToonCombatant(Combatant):
     """Represents a player's toon."""
 
-    def __init__(self) -> None:
-        self.health = 15
+    def __init__(
+        self, battle: "CogBattle", toon: Toon, deterministic: bool = False
+    ) -> None:
+        super().__init__(battle, deterministic)
+        self.health = toon.laff
+        self.selectedGag = Gag.NONE
 
     @overrides
-    def executeAttack(self, targets: List[Combatant]) -> None:
+    def executeAttack(self) -> None:
         pass
+
+    @overrides
+    def isAttackHit(self) -> bool:
+        rand = random.random()
+        isHit = rand < 0.95 and rand < Gag.CHANCE_TO_HIT[self.selectedGag]
+        print(f"The toon {'hit' if isHit else 'missed'}")
+        return isHit
