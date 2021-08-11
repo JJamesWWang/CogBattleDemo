c54c32f8fc04c3f8361ce2d16280b33649938e88
James S. Wang <jjameswwang@gmail.com>
2021-08-11 18:28:31 -0400

    Add documentation

--- gag.py
+++ gag.py
@@ -1,7 +1,25 @@
+"""Copyright 2021, James S. Wang, All rights reserved."""
+
 from typing import List, Dict
 
 
 class Gag:
+    """Namespace containing all information about gags.
+
+    Ideally, gag data would be read from a table, but here we have it hardcoded
+    for simplicity.
+
+    Attributes:
+        NONE (int): A placeholder indicating that no gag is selected.
+        PASS (int): The toon chooses to pass instead of selecting a gag.
+        SQUIRT (int): Gag that does 4 damage with a 90% chance of hitting.
+        THROW (int): Gag that does 6 damage with a 75% chance of hitting.
+        DAMAGE (dict of int to int): Maps gags to damage.
+        CHANCE_TO_HIT (dict of int to float): Maps gags to chance to hit.
+        EXECUTE_ORDER (list of int): A sorted list of gags indicating order.
+        TARGET_REQUIRED (list of int): A list of gags that require targets.
+    """
+
     NONE: int = 0
     PASS: int = 1
     SQUIRT: int = 2
