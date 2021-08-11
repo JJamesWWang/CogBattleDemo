2a44c2e5273f0621f58b93f826305b0a829eb260
James S. Wang <jjameswwang@gmail.com>
2021-08-11 18:31:06 -0400

    Add check for toons when executing attack and add docs

--- cog.py
+++ cog.py
@@ -1,3 +1,5 @@
+"""Copyright 2021, James S. Wang, All rights reserved."""
+
 from combatant import Combatant
 import random
 from typing import List, Dict
@@ -5,8 +7,16 @@ from overrides import overrides
 
 
 class Cog:
-    def __init__(self):
-        self.health = 12
+    """Represents a cog as it would in the normal gameplay mode.
+
+    Attributes:
+        health (int): Hardcoded health of the cog is set to 12. Ideally, we
+            would pass in the cog's suit and level to the constructor and
+            health would automatically be determined according to some table.
+    """
+
+    def __init__(self) -> None:
+        self.health: int = 12
 
 
 class CogCombatant(Combatant):
@@ -15,6 +25,21 @@ class CogCombatant(Combatant):
     Currently attacks, damage, and chance to hit are hardcoded; however,
     ideally these values would be read from a spreadsheet or filled through
     another more flexible method.
+
+    Args:
+        battle (CogBattle): The CogBattle that instantiates this cog.
+        cog (Cog): The cog to base this combatant off of.
+        deterministic (bool): Whether the combatant's actions are
+            deterministic. Used for unit testing.
+
+    Attributes:
+        ATTACK_A (str): Generic attack 1.
+        ATTACK_B (str): Generic attack 2.
+        ATTACKS (list of str): All attacks this cog can execute.
+        DAMAGE (dict of str to int): Damage of each attack.
+        CHANCE_TO_HIT (dict of str to float): Chance of each attack hitting,
+            from 0 to 1.
+        selectedAttack (str): The attack to use when executing an attack.
     """
 
     ATTACK_A: str = "Attack A"
@@ -32,10 +57,18 @@ class CogCombatant(Combatant):
 
     @overrides
     def executeAttack(self):
-        target = random.choice(self.battle.toons)
-        target.takeDamage(self.DAMAGE[self.selectedAttack])
+        if self.battle.toons:
+            self.selectAttack()
+            target = (
+                self.battle.toons[0]
+                if self.isDeterministic
+                else random.choice(self.battle.toons)
+            )
+            target.takeDamage(self.DAMAGE[self.selectedAttack])
 
     def selectAttack(self) -> None:
+        """Sets the selectedAttack attribute to a random attack, or the first
+        one if this combatant is set to be deterministic."""
         self.selectedAttack = (
             self.ATTACKS[0]
             if self.isDeterministic
