bf3be6ad4b705ee7de2c6a8b98baf6bd0e67170d
James S. Wang <jjameswwang@gmail.com>
2021-08-11 18:30:29 -0400

    Incorporate Combatant change and add docs

--- toon.py
+++ toon.py
@@ -1,3 +1,5 @@
+"""Copyright 2021, James S. Wang, All rights reserved."""
+
 from combatant import Combatant
 from gag import Gag
 from typing import List
@@ -6,12 +8,34 @@ import random
 
 
 class Toon:
+    """Represents a toon as it would in the normal gameplay mode.
+
+    The idea here is to separate battle logic from non-battle logic by creating
+    new objects when battles are initiated.
+
+    Attributes:
+        laff (int): Hardcoded health of the toon is set to 15. Ideally, we
+            would track the toon's laff, gag level, experience, etc.
+    """
+
     def __init__(self):
         self.laff = 15
 
 
 class ToonCombatant(Combatant):
-    """Represents a player's toon."""
+    """Represents a player's toon in combat.
+
+    Args:
+        battle (CogBattle): The CogBattle that instantiates this toon.
+        toon (Toon): The toon to base this combatant off of.
+        deterministic (bool): Whether the combatant's actions are
+            deterministic. Used for unit testing.
+
+    Attributes:
+        selectedGag (int): One of the constants in the Gag class; used to
+            determine which gag is used when executing an attack.
+        selectedTarget (Combatant): A target for the selectedGag, if necessary.
+    """
 
     def __init__(
         self, battle: "CogBattle", toon: Toon, deterministic: bool = False
@@ -23,7 +47,7 @@ class ToonCombatant(Combatant):
 
     @overrides
     def executeAttack(self) -> None:
-        if self.battle.cogs and not self.selectedTarget.isDead():
+        if self.battle.cogs and self.selectedTarget.isAlive():
             self.selectedTarget.takeDamage(Gag.DAMAGE[self.selectedGag])
 
     @overrides
